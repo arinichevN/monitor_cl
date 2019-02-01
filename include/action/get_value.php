c67db7655170d8fea4797f90cc7de88a23ff8cfb
nikolay <arinichev_n@mail.ru>
2019-02-01 09:13:38 +0300

    action function but not class added

--- include/action/get_value.php
+++ include/action/get_value.php
@@ -1,17 +1,9 @@
 <?php
 
-class get_value {
-
-    public static function getUser() {
-        return ['stranger' => '*'];
-    }
-
-    public static function execute($p) {
-        \sock\init($p['address'], $p['port'], 3);
-        $id= \acp\requestSendI1List(ACP_CMD_GET_FTS, $p['item']);
-        $data = \acp\getFTS($id);
-        \sock\suspend();
-        return $data;
-    }
-
-}
+$af = function($p) {
+	\sock\init($p['address'], $p['port'], 3);
+	$id= \acp\requestSendI1List(ACP_CMD_GET_FTS, $p['item']);
+	$data = \acp\getFTS($id);
+	\sock\suspend();
+	return $data;
+};
--- include/action/ping_peer.php
+++ include/action/ping_peer.php
@@ -1,17 +1,9 @@
 <?php
 
-class ping_peer {
-
-    public static function getUser() {
-        return ['stranger' => '*'];
-    }
-
-    public static function execute($p) {
-        \sock\init($p['address'], $p['port'], 3);
-        $id = \acp\requestSendCmd(ACP_CMD_APP_PING);
-        $data = \acp\getBufParseStateData($id);
-        \sock\suspend();
-        return $data;
-    }
-
-}
+$af = function($p) {
+	\sock\init($p['address'], $p['port'], 3);
+	$id = \acp\requestSendCmd(ACP_CMD_APP_PING);
+	$data = \acp\getBufParseStateData($id);
+	\sock\suspend();
+	return $data;
+};
